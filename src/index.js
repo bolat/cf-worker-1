d3f12550a3c0145d369b944568379287e694b936
Boulat Ashim <bolat.ashimov@gmail.com>
2025-07-30 12:35:06 +0500

    reversion 1

--- src/index.js
+++ src/index.js
@@ -7,42 +7,42 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import { google } from 'googleapis';
-import { sign } from '@tsndr/cloudflare-worker-jwt';
-
-export default {
-	async fetch(request, env, ctx) {
-		const APIkey = await env.GOOGLE_SERVICE_ACCOUNT_KEY;
-		const serviceAccountKey = JSON.parse(APIkey);
-
-		const privateKey = serviceAccountKey.private_key;
-		const clientEmail = serviceAccountKey.client_email;
-		const scopes = ['https://www.googleapis.com/auth/calendar.readonly'];
-
-		try {
-			const jwt = await sign(
-				{
-					iss: clientEmail,
-					scope: scopes.join(' '),
-					aud: 'https://oauth2.googleapis.com/token',
-					exp: Math.floor(Date.now() / 1000) + 3600,
-					iat: Math.floor(Date.now() / 1000),
-				},
-				privateKey,
-				{ algorithm: 'RS256' }
-			);
-
-			// ... остальной ваш код для обмена JWT на токен доступа и взаимодействия с Google Calendar API
-			return new Response('JWT сгенерирован успешно!');
-		} catch (error) {
-			return new Response(`Произошла ошибка: ${error.message}`, { status: 500 });
-		}
-	},
-};
+// import { google } from 'googleapis';
+// import { sign } from '@tsndr/cloudflare-worker-jwt';
 
 // export default {
 // 	async fetch(request, env, ctx) {
 // 		const APIkey = await env.GOOGLE_SERVICE_ACCOUNT_KEY;
-// 		return new Response('Hello CF Worker! ' + serviceAccountKey.client_email + ' private key: ' + serviceAccountKey.private_key);
+// 		const serviceAccountKey = JSON.parse(APIkey);
+
+// 		const privateKey = serviceAccountKey.private_key;
+// 		const clientEmail = serviceAccountKey.client_email;
+// 		const scopes = ['https://www.googleapis.com/auth/calendar.readonly'];
+
+// 		try {
+// 			const jwt = await sign(
+// 				{
+// 					iss: clientEmail,
+// 					scope: scopes.join(' '),
+// 					aud: 'https://oauth2.googleapis.com/token',
+// 					exp: Math.floor(Date.now() / 1000) + 3600,
+// 					iat: Math.floor(Date.now() / 1000),
+// 				},
+// 				privateKey,
+// 				{ algorithm: 'RS256' }
+// 			);
+
+// 			// ... остальной ваш код для обмена JWT на токен доступа и взаимодействия с Google Calendar API
+// 			return new Response('JWT сгенерирован успешно!');
+// 		} catch (error) {
+// 			return new Response(`Произошла ошибка: ${error.message}`, { status: 500 });
+// 		}
 // 	},
 // };
+
+export default {
+	async fetch(request, env, ctx) {
+		const APIkey = await env.GOOGLE_SERVICE_ACCOUNT_KEY;
+		return new Response('Hello CF Worker! ' + serviceAccountKey.client_email + ' private key: ' + serviceAccountKey.private_key);
+	},
+};
