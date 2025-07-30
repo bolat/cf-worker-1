fd04b974c9542fc87f9541f8bca222144682eb76
Boulat Ashim <bolat.ashimov@gmail.com>
2025-07-30 11:59:21 +0500

    jwt client creation attempt 1

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
+import { google } from "googleapis";
+
 
 export default {
 	async fetch(request, env, ctx) {
 		const APIkey = await env.GOOGLE_SERVICE_ACCOUNT_KEY;
 		const serviceAccountKey = JSON.parse(APIkey);
 
+		const jwtClient = new google.auth.JWT(
+			serviceAccountKey.client_email,
+			null, // private key is not used directly here
+			serviceAccountKey.private_key,
+			['https://www.googleapis.com/auth/calendar.readonly']
+		);
+		// const auth = new google.auth.GoogleAuth({
+		// 	credentials: JSON.parse(APIkey),
+		// 	scopes: ['https://www.googleapis.com/auth/calendar.readonly'],
+		// });
 		return new Response('Hello CF Worker! ' + serviceAccountKey.client_email + ' private key: ' + serviceAccountKey.private_key);
 	},
 };
